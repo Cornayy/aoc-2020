bc7e1ce0a784f65dbf9e537f0f068063bb291a5b
Cornayy <38261196+Cornayy@users.noreply.github.com>
2020-12-10 10:03:03 +0100

    feat: setup day 10

---
+++ src/days/10/10.ts
@@ -0,0 +1,16 @@
+import { IReader } from './../../parsing/IReader';
+import { Day } from '../../Day';
+
+export class Day10 extends Day {
+    constructor(reader: IReader) {
+        super(reader, 10);
+    }
+
+    public solveA(): number {
+        throw new Error('Method not implemented.');
+    }
+
+    public solveB(): number {
+        throw new Error('Method not implemented.');
+    }
+}
